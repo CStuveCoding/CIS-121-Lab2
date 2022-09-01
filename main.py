76b361f2bbd8802ea2c5533aebe8b1c15f4aab73
CStuveCode <112113678+CStuveCoding@users.noreply.github.com>
2022-09-01 21:30:46 +0000

    I did some crazy calculations to find someones estimated final height along with how old they would be in dog, cat, and horse years

--- main.py
+++ main.py
@@ -9,19 +9,30 @@ dog_age_decimal = dog_age % 1 #isolates the decimal in the dog age
 dog_age_total_days = dog_age_decimal * 360 #puts that decimal into days
 dog_age_months = int((dog_age_total_days / 30) / 1) #calculates months from the decimal
 dog_age_days = int(((dog_age_total_days / 30) % 1) * 30) #calculates days from the decimal
-print("Hi",first_name,"",last_name,",you are",age_years,"years old. You are also",dog_age_years,"year(s),",dog_age_months,"month(s), and ",dog_age_days,"days old in dog years.")
+print("Hi",first_name,"",last_name,",you are",age_years,"years old. You are also",dog_age_years,"year(s),",dog_age_months,"month(s), and ",dog_age_days,"day(s) old in dog years.")
 horse_age = ((((age**2)-47)/7)+12)*3
 horse_age_years = int(horse_age / 1) #isolates horse years
 horse_age_decimal = horse_age % 1 #isolates the decimal in the horse age 
 horse_age_total_days = horse_age_decimal * 360 #puts that decimal into days
 horse_age_months = int((horse_age_total_days / 30) / 1) #calculates months from the decimal
 horse_age_days = int(((horse_age_total_days / 30) % 1) * 30) #calculates days from the decimal
-print("Hi",first_name,"",last_name,",you are",age_years,"years old. You are also",horse_age_years,"year(s),",horse_age_months,"month(s), and ",horse_age_days,"days old in horse years.")
-mother_height = float(input("What is your Mother's height?"))
-father_height = float(input("What is your Father's height?"))
-sex = input("What is your biological sex?")
-if sex == male or Male:
-  ((mother_height + father_height)+5)/2
-else:
-  ((mother_height + father_height)-5)/2
-                            
+print(first_name,",you are",horse_age_years,"year(s),",horse_age_months,"month(s), and ",horse_age_days,"day(s) old in horse years.")
+cat_age = age/9
+cat_age_years = int(cat_age / 1) #isolates horse years
+cat_age_decimal = cat_age % 1 #isolates the decimal in the horse age 
+cat_age_total_days = cat_age_decimal * 360 #puts that decimal into days
+cat_age_months = int((cat_age_total_days / 30) / 1) #calculates months from the decimal
+cat_age_days = int(((cat_age_total_days / 30) % 1) * 30) #calculates days from the decimal
+print(first_name,",you are",cat_age_years,"year(s),",cat_age_months,"month(s), and ",cat_age_days,"day(s) old in cat years.")
+mother_height = float(input("What is your mother's height in inches?\t"))
+father_height = float(input("What is your father's height in inches?\t"))
+estimated_height = 0
+while estimated_height == 0:
+ sex = input("What is your biological sex?\t")
+ if sex == "Male":
+  estimated_height = ((mother_height + father_height)+5)/2
+ elif sex == "Female":
+  estimated_height = ((mother_height + father_height)-5)/2
+ else:
+  print("Please use inputs Male or Female")
+print(first_name,"you are",age_years,"years old and your estimated height is",estimated_height,"inches.")
